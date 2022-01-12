b8aa60f062752986d7f0284c07ff1070c6ebacd6
Jade <whit1088@algonquinlive.com>
2022-01-11 20:36:47 -0500

    Excercise 1-2

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,13 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const say = (message) => console.log(message);
+const students = require('./students.json');
+
+students.forEach(student => {
+  say(`Hello ${student.firstName} ${student.lastName}!`)
+})
+
+const filterName = students.filter((student) => student.lastName.toLowerCase().indexOf("d") === 0);
+say("Count of last names starting with D is " + filterName.length + ".");
